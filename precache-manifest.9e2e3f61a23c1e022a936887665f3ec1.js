2167da7a1f8b2c25bb190855ab5856905c2a90c3
Biswa24 <sah.biswajeet@gmail.com>
2020-06-17 18:08:28 +0530

    Updates

---
+++ precache-manifest.9e2e3f61a23c1e022a936887665f3ec1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b747a3105def52019b750e481101831",
+    "url": "/Image_search/index.html"
+  },
+  {
+    "revision": "6a81616861e3644db61b",
+    "url": "/Image_search/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "da12a852da3722c35911",
+    "url": "/Image_search/static/js/2.f57ee317.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Image_search/static/js/2.f57ee317.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a81616861e3644db61b",
+    "url": "/Image_search/static/js/main.58144a16.chunk.js"
+  },
+  {
+    "revision": "c51618bcecac665ea9f3",
+    "url": "/Image_search/static/js/runtime-main.9bc58af0.js"
+  }
+]);
